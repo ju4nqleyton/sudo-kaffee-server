5f3780f4fe3b0bf01052ac204bcb22b694f850a7
ju4nqleyton <ju4nqleyton@gmail.com>
2025-05-16 10:21:24 -0500

    conexion a mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('MONGODB ON');
+  })
+  .catch((error) => console.error(error));
 
 const PORT = 3000;
 app.listen(PORT, () => {
